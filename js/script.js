cfd2ca85e8016bb4e386d806c9305830cd24fe03
James Crawley <jamescrawley@James-ZCW-15.local>
2016-09-29 12:56:17 -0400

    Finished numeric loops. Refactored CSS for bigger buttons.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,81 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function oneToTen (){
+    var oneToTen = "";
+    for (var i = 1; i < 11; i++ ) {
+        oneToTen += i  + " ";
+    }
+
+    display.innerHTML = oneToTen;
+
+}
+
+function oddNumbers () {
+    var oddNums = "";
+    for (var i = 1; i < 20; i += 2) {
+        oddNums += i + " ";
+    }
+
+    display.innerHTML = oddNums;
+
+}
+
+function squareNumbers () {
+
+    var squareNums = "";
+    for (var i = 1; i < 11; i++ ) {
+        squareNums += Math.pow(i, 2) + " ";
+    }
+
+    display.innerHTML = squareNums;
+
+}
+
+function randomNumbers () {
+
+    var randNums = "";
+    for (var i = 0; i < 4; i++) {
+        randNums += Math.floor((Math.random() * 100) + 1) + " ";
+    }
+
+    display.innerHTML = randNums;
+
+}
+
+function evenNumbers (nums) {
+    var evenNums = "";
+    for (var i = 2; i < nums; i += 2) {
+        evenNums += i + " ";
+    }
+
+    display.innerHTML = evenNums;
+
+}
+
+function powersOfTwo (nums) {
+
+    var powerNums = "";
+    for (var i = 0; i <= nums; i++) {
+        powerNums += Math.pow(2, i) + " ";
+    }
+
+    display.innerHTML = powerNums;
+
+}
+
+function areWeThereYet () {
+
+}
+
+function triangle () {
+
+}
+
+function tableSquare () {
+
+}
+
+function tableSquares () {
+
 }
